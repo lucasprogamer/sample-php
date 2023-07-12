4a45387ef8f8705a1bef12782f0d6612d526672a
Lucas | Alves <lucasouzaweb@gmail.com>
2023-07-12 13:41:46 -0300

    feat: add middleware to routes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Controllers\ContactController;
-use App\Controllers\UserController;
 use Src\Router\Route;
+use App\Middleware\AuthMiddleware;
+use App\Controllers\UserController;
+use App\Controllers\ContactController;
 
 Route::get('/', function () {
     echo 'Hello, world!';
 });
-
+Route::post('/auth', ['controller' => UserController::class, 'method' => 'auth']);
 Route::get('/users', ['controller' =>  UserController::class, 'method' => 'index']);
 Route::post('/users', ['controller' =>  UserController::class, 'method' => 'create']);
 Route::get('/users/{id}/contacts', ['controller' =>  UserController::class, 'method' => 'getWithContacts']);
@@ -15,7 +16,7 @@ Route::get('/users/{name}/name', ['controller' =>  UserController::class, 'metho
 Route::put('/users/{id}', ['controller' =>  UserController::class, 'method' => 'update']);
 Route::delete('/users/{id}', ['controller' =>  UserController::class, 'method' => 'delete']);
 
-Route::get('/contacts', ['controller' =>  ContactController::class, 'method' => 'index']);
+Route::get('/contacts', ['controller' =>  ContactController::class, 'method' => 'index', 'middleware' => AuthMiddleware::class]);
 Route::get('/contacts/{id}', ['controller' =>  ContactController::class, 'method' => 'get']);
 Route::post('/contacts', ['controller' =>  ContactController::class, 'method' => 'create']);
 Route::put('/contacts/{id}', ['controller' =>  ContactController::class, 'method' => 'update']);
